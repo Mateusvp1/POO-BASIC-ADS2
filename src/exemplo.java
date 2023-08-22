1d28812bf08d49ce27aac7fdcf99ee4341d0b37e
Mateusvp1 <mateusvp16@gmail.com>
2023-08-22 19:27:22 -0300

    exmplo

---
+++ src/exemplo.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class exemplo {
+    public static void main(String[] args) {
+        String nome = JOptionPane.showInputDialog(null, "Digite o Nome :", "Entrada", JOptionPane.QUESTION_MESSAGE);
+        System.out.println(nome);
+        String sobrenome = JOptionPane.showInputDialog(null, "Digite o Sobrenome :", "Entrada", JOptionPane.QUESTION_MESSAGE);
+        System.out.println(sobrenome);
+        String resultado = ""+nome +" "+sobrenome;
+        JOptionPane.showMessageDialog(null, "Esse é o seu nome completo :"+resultado , "Mensagem", JOptionPane.INFORMATION_MESSAGE);
+    }
+}
